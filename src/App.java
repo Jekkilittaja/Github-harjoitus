00eb10861475682b32fcf3a9d13f21e397cc9058
Otto Iiro Viljami Kiviharju <viljami.kiviharju@gmail.com>
2024-10-10 20:12:58 +0300

    Muistipeli v1.1
    Lisättin nums randomizer ja tulostetaan tyhjiä riveä tietyn viiveen jälkeen.

--- src/App.java
+++ src/App.java
@@ -8,5 +8,23 @@ public class App {
         int[] nums = new int[7];
         int[] ans = new int[7];
         
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = random.nextInt(6) + 1;
+        }
+        try {
+            for (int i = 0; i < nums.length; i++) {
+                System.out.print(nums[i] + " ");
+            }
+            Thread.sleep(3000);
+            for(int i = 0; 1 < 20; i++){
+                System.out.println();
+                if (i == 19) {
+                    break;
+                }
+            }
+        }
+        catch(InterruptedException ex) { 
+            Thread.currentThread().interrupt();
+        }
     }
 }
